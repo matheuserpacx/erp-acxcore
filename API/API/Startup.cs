317f70b7dd2a710267afec2bc0f47c7809fc6641
hugo-acx <hugo.falcao@erpacx.com>
2022-04-13 15:11:54 -0300

    a

--- API/API/Startup.cs
+++ API/API/Startup.cs
@@ -40,7 +40,7 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
