b35343c88f6eb4fdebd39dbbd638683f8a8531b9
hugo-acx <hugo.falcao@erpacx.com>
2022-04-13 15:21:14 -0300

    Add DBConfig

--- API/API/Startup.cs
+++ API/API/Startup.cs
@@ -49,7 +49,7 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 //endpoints.MapHealthChecks("/healthz").RequireAuthorization();
-                endpoints.MapControllers();
+                //endpoints.MapControllers();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
